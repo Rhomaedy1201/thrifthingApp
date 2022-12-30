3b0f63136c8a26108f0b7b3b8aa21d6d6a13d962
Muhammad Rhomaedi <57169929+Rhomaedy1201@users.noreply.github.com>
2022-12-30 17:38:39 +0700

    mengembalikan file

---
+++ lib/app/repositorys/repo_produk.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import '/app/Pages/detailProductPage.dart';
+import '/app/models/model_produk.dart';
+
+class RepositoryProduk {
+  final _baseUrl =
+      "http://localhost/restApi_goThrift/produk_user/get_produk_user.php";
+
+  DetailProductPage detailPage = DetailProductPage();
+
+  Future getData() async {
+    try {
+      final response = await http.get(Uri.parse(_baseUrl));
+      if (response.statusCode == 200) {
+        Iterable it = jsonDecode(response.body)[0]['result'];
+        List<ProdukUser> produk =
+            it.map((e) => ProdukUser.fromJson(e)).toList();
+        return produk;
+      } else {
+        print("salah lohhhh");
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
